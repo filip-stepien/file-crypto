a9f2e2da94b5074492c1afa2ac97142599c13fcd
Filip Stępień <filip.stepien30@gmail.com>
2025-11-05 16:43:04 +0100

    chore: extract password input to own controller & xml

--- src/main/java/xyz/cursedman/filecrypto/controllers/AlgorithmSettingsController.java
+++ src/main/java/xyz/cursedman/filecrypto/controllers/AlgorithmSettingsController.java
@@ -1,43 +1,4 @@
 package xyz.cursedman.filecrypto.controllers;
 
-import javafx.fxml.FXML;
-import javafx.scene.control.Button;
-import javafx.scene.control.PasswordField;
-import javafx.scene.control.TextField;
-
 public class AlgorithmSettingsController {
-    @FXML
-    private PasswordField passwordField;
-
-    @FXML
-    private TextField passwordTextField;
-
-    @FXML
-    private Button showPasswordButton;
-
-    @FXML
-    private void initialize() {
-        passwordTextField.textProperty().bindBidirectional(passwordField.textProperty());
-    }
-
-    @FXML
-    private void togglePasswordVisibility() {
-        if (passwordTextField.isVisible()) {
-            passwordField.setVisible(true);
-            passwordField.setManaged(true);
-
-            passwordTextField.setVisible(false);
-            passwordTextField.setManaged(false);
-
-            showPasswordButton.setText("Show");
-        } else {
-            passwordField.setVisible(false);
-            passwordField.setManaged(false);
-
-            passwordTextField.setVisible(true);
-            passwordTextField.setManaged(true);
-
-            showPasswordButton.setText("Hide");
-        }
-    }
 }
